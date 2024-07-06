5dff92c9ea951546767e976350fcc42cdedd9cc1
Nitika13 <ahlawatnitika4@gmail.com>
2024-07-06 16:19:11 +0530

    update

---
+++ client/script.js
@@ -0,0 +1,4 @@
+document.getElementById('scrollButton').addEventListener('click', function() {
+    // Smooth scroll to the target element
+    document.getElementById('target').scrollIntoView({ behavior: 'smooth' });
+});
